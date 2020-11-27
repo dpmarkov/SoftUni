9ac0af3f186f72dbddcb81eba8953d45e15da9a1
dpmarkov <71282381+dpmarkov@users.noreply.github.com>
2020-11-27 18:51:54 +0200

    Create matchFullName.js

---
+++ JavaScript Fundamentals/27.regular_expressions-lab/matchFullName.js
@@ -0,0 +1,15 @@
+function matchFullName(input) {
+    let pattern = /\b[A-Z][a-z]+ [A-Z][a-z]+\b/g;
+    
+    let arr = pattern.exec(input);
+    let resultArr = []
+
+    while (arr != null){
+        resultArr.push(arr[0]);
+        arr = pattern.exec(input);
+    }
+
+    console.log(resultArr.join(' '));
+}
+
+matchFullName("Ivan Ivanov, Ivan ivanov, ivan Ivanov, IVan Ivanov, Test Testov, Ivan	Ivanov")
