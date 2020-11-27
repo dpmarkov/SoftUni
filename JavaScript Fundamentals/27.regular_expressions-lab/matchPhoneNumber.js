527844380a4e66eced784ac82a468e49e70d7d1c
dpmarkov <71282381+dpmarkov@users.noreply.github.com>
2020-11-27 21:20:52 +0200

    Create matchPhoneNumber.js

---
+++ JavaScript Fundamentals/27.regular_expressions-lab/matchPhoneNumber.js
@@ -0,0 +1,8 @@
+function matchPhoneNumber([input]) {
+    let pattern = /\+359([-, ])2\1[0-9]{3}\1[0-9]{4}\b/g;
+    console.log(input.match(pattern).join(', '));
+}
+
+matchPhoneNumber(
+    "+359 2 222 2222,359-2-222-2222, +359/2/222/2222, +359-2 222 2222 +359 2-222-2222, +359-2-222-222, +359-2-222-22222 +359-2-222-2222"
+)
