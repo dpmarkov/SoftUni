fc7d7fcc724d89451c432e0e1cd885280f2261c7
dpmarkov <71282381+dpmarkov@users.noreply.github.com>
2020-12-23 19:13:31 +0200

    Create concatinateData.js

---
+++ JavaScript Basics/02.first_steps_in_programming-exercise/concatinateData.js
@@ -0,0 +1,10 @@
+function concatinateData(a,b,c,d){
+    let fName = a;
+    let lName = b;
+    let age = Number(c);
+    let town = d;
+
+    console.log(`You are ${fName} ${lName}, a ${age}-years old person from ${town}.`);
+}
+
+concatinateData("Dimitar", "Markov", "29", "Varna");
