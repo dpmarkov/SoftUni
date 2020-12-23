03ae96c0675f180d051b9b4a1d4a4db5b674b3a7
dpmarkov <71282381+dpmarkov@users.noreply.github.com>
2020-12-23 19:13:53 +0200

    Create projects.js

---
+++ JavaScript Basics/02.first_steps_in_programming-exercise/projects.js
@@ -0,0 +1,6 @@
+function projects(a,b){
+    let name = a;
+    let hours = Number(b) * 3
+    console.log(`The architect ${name} will need ${hours} hours to complete ${b} project/s.`);
+}
+projects("Dimitar",'9');
