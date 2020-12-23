72c63cd8fc1a4752c3de9c78e376a837c2d930c4
dpmarkov <71282381+dpmarkov@users.noreply.github.com>
2020-12-23 19:12:31 +0200

    Create printNumberToTen.js

---
+++ JavaScript Basics/01.first_steps_in_programming-lab/printNumberToTen.js
@@ -0,0 +1,14 @@
+function printNumberToTen(){
+    num = 1;
+    console.log(num++);
+    console.log(num++);
+    console.log(num++);
+    console.log(num++);
+    console.log(num++);
+    console.log(num++);
+    console.log(num++);
+    console.log(num++);
+    console.log(num++);
+    console.log(num++);
+  }
+  printNumberToTen();
