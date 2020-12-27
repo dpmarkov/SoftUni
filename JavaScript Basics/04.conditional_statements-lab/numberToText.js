63d9aa92ef048831f2e782876391071666b5837c
dpmarkov <71282381+dpmarkov@users.noreply.github.com>
2020-12-27 14:11:28 +0200

    Create numberToText.js

---
+++ JavaScript Basics/04.conditional_statements-lab/numberToText.js
@@ -0,0 +1,34 @@
+function numberToText(x) {
+    let y = Number(x);
+    if (y == 1) {
+        console.log("one");
+    }
+    else if (y == 2) {
+        console.log("two");
+    }
+    else if (y == 3) {
+        console.log("three");
+    }
+    else if (y == 4) {
+        console.log("four");
+    }
+    else if (y == 5) {
+        console.log("five");
+    }
+    else if (y == 6) {
+        console.log("six");
+    }
+    else if (y == 7) {
+        console.log("seven");
+    }
+    else if (y == 8) {
+        console.log("eight");
+    }
+    else if (y == 9) {
+        console.log("nine");
+    }
+    else {
+        console.log("number too big");
+    }
+}
+numberToText(4);
