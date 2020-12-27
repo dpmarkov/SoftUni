e61d2726a8bc3795dd49a97bdf208d59c5a63d4e
dpmarkov <71282381+dpmarkov@users.noreply.github.com>
2020-12-27 14:11:02 +0200

    Create greaterNumber.js

---
+++ JavaScript Basics/04.conditional_statements-lab/greaterNumber.js
@@ -0,0 +1,12 @@
+function greaterNumber(num1,num2){
+    if (num1>num2){
+        console.log(num1);
+    }
+    else if(num1==num2){
+        console.log(num1);    
+    }
+    else{
+        console.log(num2);     
+    }
+}
+greaterNumber(5,5);
