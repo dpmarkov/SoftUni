69b439dba3ad5ead26fb2caeaac07233ca523081
dpmarkov <71282381+dpmarkov@users.noreply.github.com>
2020-12-27 14:11:14 +0200

    Create passwordGuess.js

---
+++ JavaScript Basics/04.conditional_statements-lab/passwordGuess.js
@@ -0,0 +1,10 @@
+function passwordGuess(input){
+    if (input === "s3cr3t!P@ssw0rd"){
+        console.log("Welcome");
+    }
+    else {
+        console.log("Wrong password!");
+    }
+}
+passwordGuess("testtt");
+passwordGuess("s3cr3t!P@ssw0rd");
