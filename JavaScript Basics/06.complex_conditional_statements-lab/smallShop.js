373d44c93240b9d91480ea98917673533da55f06
dpmarkov <71282381+dpmarkov@users.noreply.github.com>
2020-12-29 19:27:19 +0200

    Create smallShop.js

---
+++ JavaScript Basics/06.complex_conditional_statements-lab/smallShop.js
@@ -0,0 +1,63 @@
+function smallShop (a,b,c){
+    let product = a;
+    let town = b;
+    let quantity = Number(c);
+    let result = 0;
+    if (town === "Sofia"){
+        switch(product){
+            case "coffee":
+                result = quantity * 0.50;
+                break;
+            case "water":
+                result = quantity * 0.80;
+                break;
+            case "beer":
+                result = quantity * 1.20;
+                break;
+            case "sweets":
+                result = quantity * 1.45;
+                break;    
+            case "peanuts":
+                result = quantity * 1.60;
+                break;    
+        }
+    } else if (town === "Plovdiv"){
+        switch(product){
+            case "coffee":
+                result = quantity * 0.40;
+                break;
+            case "water":
+                result = quantity * 0.70;
+                break;
+            case "beer":
+                result = quantity * 1.15;
+                break;
+            case "sweets":
+                result = quantity * 1.30;
+                break; 
+            case "peanuts":
+                result = quantity * 1.50;
+                break;   
+        }    
+    } else {
+        switch(product){
+            case "coffee":
+                result = quantity * 0.45;
+                break;
+            case "water":
+                result = quantity * 0.70;
+                break;
+            case "beer":
+                result = quantity * 1.10;
+                break;
+            case "sweets":
+                result = quantity * 1.35;
+                break; 
+            case "peanuts":
+                result = quantity * 1.55;
+                break;   
+        }
+    }
+    console.log(result);
+}
+//smallShop("coffee", "Varna", "2");
