b790b5a2fe8c4da3430080ef0c31e1fe2c527beb
dpmarkov <71282381+dpmarkov@users.noreply.github.com>
2020-12-31 03:42:17 +0200

    Create vowelsSum.js

---
+++ JavaScript Basics/08.for_loop-lab/vowelsSum.js
@@ -0,0 +1,15 @@
+function vowelsSum(arg){
+    let text = arg;
+    let result = 0;
+    for(let i=0; i<text.length; i++){
+        switch(text[i]){
+            case "a": result += 1; break;
+            case "e": result += 2; break;
+            case "i": result += 3; break;
+            case "o": result += 4; break;
+            case "u": result += 5; break;
+        }
+    }
+    console.log(result);
+}
+//vowelsSum("hello");
