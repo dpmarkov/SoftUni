c659655c95d8e4f8a6fa751b0ae12073f67be0d4
dpmarkov <71282381+dpmarkov@users.noreply.github.com>
2020-12-31 03:42:12 +0200

    Create characterSequence.js

---
+++ JavaScript Basics/08.for_loop-lab/characterSequence.js
@@ -0,0 +1,9 @@
+function characterSequence(arg){
+    let text = arg;
+    let length = text.length;
+
+    for(let i = 0; i < length; i++){
+        console.log(text[i]);
+    }
+}
+//characterSequence("SoftUni");
