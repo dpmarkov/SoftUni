4fba37951a7ec82e4e429aba7ebf446acf3b46e4
dpmarkov <71282381+dpmarkov@users.noreply.github.com>
2021-01-12 13:46:45 +0200

    Create aggregateElements.js

---
+++ JavaScript Advanced/02.syntax_functions_and_statements-lab/aggregateElements.js
@@ -0,0 +1,39 @@
+function aggregateElements(arr){
+    function sum(arr){
+        let result = 0;
+
+        arr.forEach(
+            x => result += x
+        )
+
+        return result;
+    }
+
+    function sumI(arr){
+        let result = 0;
+        
+        arr.forEach(
+            x => result += 1/x
+        )
+
+        return result;
+    }
+
+    function concat(arr){
+        let result = '';
+        
+        arr.forEach(
+            x => result += x
+        )
+
+        return result;
+    }
+
+    console.log(sum(arr));
+    console.log(sumI(arr));
+    console.log(concat(arr));
+}
+
+aggregateElements([1, 2, 3])
+console.log('---');
+aggregateElements([2, 4, 8, 16])
