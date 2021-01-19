8dc2a637dd1c1e687ea5aa57127e7032ba7c1bc7
dpmarkov <71282381+dpmarkov@users.noreply.github.com>
2021-01-19 02:51:14 +0200

    Create cityRecord.js

---
+++ JavaScript Advanced/05.objects_and_composition-lab/cityRecord.js
@@ -0,0 +1,11 @@
+function cityRecord(name, population, treasury) {
+    return {
+        name,
+        population,
+        treasury
+    };
+}
+
+console.log(cityRecord('Tortuga', 7000, 15000));
+console.log('---');
+console.log(cityRecord('Santo Domingo', 12000, 23500));
