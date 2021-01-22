e2044f437b8aa215fbbe50ad2188e2b9170378b4
dpmarkov <71282381+dpmarkov@users.noreply.github.com>
2021-01-23 00:44:43 +0200

    format the text

---
+++ JavaScript Advanced/07.dom_introduction-lab/format_the_text/solution.js
@@ -0,0 +1,35 @@
+function solve() {
+  let inputElement = document.getElementById('input');
+  let inputArr = inputElement.innerHTML.split('. ');
+  
+  //adding dots where needed..................................................................................................
+  if (inputArr.length > 1){
+    for (let i=0; i<inputArr.length-1; i++){
+      inputArr[i] += '.';
+    }
+  }
+  //..................................................................................................adding dots where needed
+  
+  let paragraphs = [[]];
+  let counter = 1;
+  let pIndex = 0;
+
+  for (let sentence of inputArr){
+    if (counter == 4){
+      pIndex++;
+      counter = 1;
+      paragraphs[pIndex] = [];
+    }
+
+    paragraphs[pIndex].push(sentence);
+
+    counter++;
+  }
+
+  paragraphs.forEach(x => {
+    let newParagraph = document.createElement('p');
+    newParagraph.innerHTML = x.join(' ');
+    document.getElementById('output').appendChild(newParagraph);
+  });
+}
+  
