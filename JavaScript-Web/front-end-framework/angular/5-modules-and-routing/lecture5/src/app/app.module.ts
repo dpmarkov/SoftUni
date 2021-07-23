baa4de65345f979b160a1f0bbf9f1ea789ff8ea3
dpmarkov <71282381+dpmarkov@users.noreply.github.com>
2021-07-24 02:13:52 +0300

    created routing

--- JavaScript-Web/front-end-framework/angular/5-modules-and-routing/lecture5/src/app/app.module.ts
+++ JavaScript-Web/front-end-framework/angular/5-modules-and-routing/lecture5/src/app/app.module.ts
@@ -4,16 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CoreModule
+    CoreModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
