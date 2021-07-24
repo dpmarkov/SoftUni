755bbcccc9d1ebb59eca3dfb85a0f450549fc0d0
dpmarkov <71282381+dpmarkov@users.noreply.github.com>
2021-07-25 02:30:10 +0300

    sub routes

--- JavaScript-Web/front-end-framework/angular/5-modules-and-routing/lecture5.2/src/app/app.module.ts
+++ JavaScript-Web/front-end-framework/angular/5-modules-and-routing/lecture5.2/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { UserModule } from './user/user.module';
 import { AboutComponent } from './about/about.component';
 import { AppRoutingModule } from './app-routing.module';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { TestModule } from './test/test.module';
 
 export const myStringInjectionToken = new InjectionToken('myString');
 export const myStringInjectionToken2 = new InjectionToken('myString2');
@@ -24,7 +25,8 @@ export const myStringInjectionToken2 = new InjectionToken('myString2');
     CoreModule,
     SharedModule,
     UserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TestModule
   ],
   providers: [
     {
--- JavaScript-Web/front-end-framework/angular/5-modules-and-routing/lecture5.2/src/app/test/test.module.ts
+++ JavaScript-Web/front-end-framework/angular/5-modules-and-routing/lecture5.2/src/app/test/test.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OneComponent } from './one/one.component';
 import { TwoComponent } from './two/two.component';
-
-
+import { MainComponent } from './main/main.component';
+import { TestRoutingModule } from './test-routing.module';
 
 @NgModule({
   declarations: [
     OneComponent,
-    TwoComponent
+    TwoComponent,
+    MainComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    TestRoutingModule
   ]
 })
 export class TestModule { }
